cd24315aa9d1c2543c0a60f5a5b8fa12d7f5c945
拖拉斯基~ <94552711+SnowTlsj@users.noreply.github.com>
2022-10-25 16:57:59 +0800

    Rabbit

--- jd_racx_2022.py
+++ jd_racx_2022.py
@@ -2,19 +2,31 @@
 new Env('热爱穿行-2022');
 变量: RabbitToken
 export RabbitToken="token值"
+export CXJ_THREADS_NUMS=1
+export CXJ_PINS="pin1,pin2,pin3"
+export CXJ_NUM="0"
+export CXJ_RAISE="1"
+
+
+CXJ_THREADS_NUMS: 并发数
+CXJ_PIN: 需要跑的ck的pin值
+CXJ_NUM: 需要的前多少个号为车头
+CXJ_RAISE: 是否自动升级，1：为自动升级，0为不自动升级
 
 log剩余次数大于5000方可使用
 '''
 
 import os
 
-BING_FA_STATUS = False
+if not os.environ.get("LogHost"):
+    print("请先填写LogHost变量\nexport LogHost='xxxxx'")
+    exit()
+
 try:
     CXJ_THREADS_NUM = int(os.environ.get("CXJ_THREADS_NUMS", 1))
 except:
     CXJ_THREADS_NUM = 1
 if CXJ_THREADS_NUM > 1:
-    BING_FA_STATUS = True
     try:
         from gevent import monkey
 
@@ -31,7 +43,7 @@ if CXJ_THREADS_NUM > 1:
 
         monkey.patch_all()
     except:
-        BING_FA_STATUS = False
+        print("缺少依赖，无法并发")
 try:
     import requests
     import Crypto
@@ -45,4 +57,4 @@ except ImportError:
 from MR_util.racx_task_2022 import main
 
 if __name__ == '__main__':
-    main(BING_FA_STATUS)
+    main()
